4f440a79b78dcefe8f2fdcdee5f39469bd98c13d
IT23656338 <it23656338@my.sliit.lk>
2026-04-05 21:50:38 +0530

    feat: update ESLint configuration with additional TypeScript rules and remove backup Overview file

--- vehicle-market-admindash/eslint.config.js
+++ vehicle-market-admindash/eslint.config.js
@@ -23,6 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-empty': 'warn',
+      'react-hooks/set-state-in-effect': 'warn'
     },
   }
 );
